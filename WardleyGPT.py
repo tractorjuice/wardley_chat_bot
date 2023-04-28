85361e522a64c3e86168bf041238bbff49321513
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-28 21:40:09 +0100

    Update WardleyGPT.py

--- WardleyGPT.py
+++ WardleyGPT.py
@@ -47,7 +47,7 @@ if 'generated' not in st.session_state:
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
-query = st.text_input("Question: ", "What is Wardley Mapping?", key="input")
+query = st.text_input("Question: ", "What questions can I ask?", key="input")
 
 if 'messages' not in st.session_state:
     st.session_state['messages'] = get_initial_message()
