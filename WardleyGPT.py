f45b0994e53ce6ac22ce442650f5c105f18abf74
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-28 21:43:43 +0100

    Update WardleyGPT.py

--- WardleyGPT.py
+++ WardleyGPT.py
@@ -6,7 +6,8 @@ import openai
 st.set_page_config(page_title="Chat with WardleyGPT")
 st.title("Chat with WardleyGPT")
 st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
-st.sidebar.markdown("Current Version: 0.0.1")
+st.sidebar.markdown("Current Version: 0.1.4")
+st.sidebar.markdown("Using GPT-4 API)
 st.sidebar.markdown("Not optimised")
 st.sidebar.markdown("May run out of OpenAI credits")
 
